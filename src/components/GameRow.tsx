5295503c778c5e364cb2b695ac441f75c62d957a
Gabriel Macario <gmacarioweb@gmail.com>
2024-07-24 01:16:59 -0700

    Use Answer and AnswerMap Contexts and conditionally render LetterBoxes

--- src/components/GameRow.tsx
+++ src/components/GameRow.tsx
@@ -1,5 +1,7 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useContext } from 'react';
 import LetterBox from "./LetterBox";
+import { AnswerContext, AnswerMapContext } from '../contexts/GameState';
+import LetterStatus from '../enums/LetterStatus';
 
 interface GameRowProps {
     children?: React.ReactNode;
@@ -9,17 +11,35 @@ interface GameRowProps {
 const GameRowStyles: CSSProperties = {
     width: "100%",
     display: "flex",
-    justifyContent: "space-evenly"
+    justifyContent: "space-between"
 };
 
-const GameRow: React.FC<GameRowProps> = ({ children, word = '' }: GameRowProps) => {
+const GameRow: React.FC<GameRowProps> = ({ word = '' }: GameRowProps) => {
+    const answerMap = useContext(AnswerMapContext);
+    const answer = useContext(AnswerContext);
+    
     let boxes = [];
 
-    for (let i = 0; i < word.length; i++ ) {
-        boxes.push(<LetterBox letter={word[i]} />)
+    for (let i = 0; i < word.length; i++) {
+        let status: LetterStatus;
+
+        if (word[i] === answer[i]) {
+            status = LetterStatus.exact;
+        } else if (answerMap.get(word[i])) {
+            status = LetterStatus.contains;
+        } else {
+            status = LetterStatus.incorrect;
+        }
+
+        boxes.push(
+            <LetterBox
+                letter={word[i]}
+                letterStatus={status}
+            />
+        )
     }
     for (let i = word.length; i < 5; i++) {
-        boxes.push(<LetterBox/>);
+        boxes.push(<LetterBox />);
     }
 
     return (
