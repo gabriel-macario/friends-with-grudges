1cc7b61c47dcdec191b348a0f9e508b4f86a438d
Gabriel Macario <gmacarioweb@gmail.com>
2024-07-24 01:15:08 -0700

    Initialize and provide Answer and AnswerMap Contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import LetterBox from "./components/LetterBox";
 import GameBoard from "./components/GameBoard";
+import { AnswerContext, AnswerMapContext } from "./contexts/GameState";
 
 const App: React.FC = () => {
+  const answer = "tests".toUpperCase();
+
+  let answerChars = [...answer];
+
+  let answerMap = new Map();
+
+  answerChars.forEach(char => answerMap.set(char, true));
+
   return (
-    <div>
-      <h1 style={{
-        textAlign: "center"
-      }}>Wordle Clone</h1>
-      <GameBoard gameFinished={false} />
-    </div>
+    <AnswerContext.Provider value={answer}>
+      <AnswerMapContext.Provider value = {answerMap}>
+        <div>
+          <h1 style={{
+            textAlign: "center"
+          }}>Wordle Clone</h1>
+          <GameBoard gameFinished={false} />
+        </div>
+      </AnswerMapContext.Provider>
+    </AnswerContext.Provider>
   )
 }
 // function App() {
