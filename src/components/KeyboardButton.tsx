797fcd0ca4cb5faac526534ec72133651964a04c
Gabriel Macario <gmacarioweb@gmail.com>
2024-07-25 16:38:10 -0700

    Scaffold KeyboardButton

---
+++ src/components/KeyboardButton.tsx
@@ -0,0 +1,24 @@
+import React, { CSSProperties } from "react";
+
+interface KeyboardButtonProps {
+    key: string;
+    keyStatus?: string;
+};
+
+const KeyboardButton: React.FC<KeyboardButtonProps> = ({ key, keyStatus }: KeyboardButtonProps) => {
+    const KeyboardButtonStyles: CSSProperties = {
+        backgroundColor: "#d3d6dA",
+        borderRadius: "4px",
+        color: "black",
+        height: "58px",
+        width: "30.5px"
+    }
+
+    return <button
+        style={KeyboardButtonStyles}
+    >
+        {key}
+    </button>
+}
+
+export default KeyboardButton;
