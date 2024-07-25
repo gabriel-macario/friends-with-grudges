4184b27736b80648c05024697f629453a9205960
Gabriel Macario <gmacarioweb@gmail.com>
2024-07-24 21:39:14 -0700

    Implement isEnter helper function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,7 @@
 export function isLetter(e: KeyboardEvent): Boolean {
-
     return e.code === `Key${e.key.toUpperCase()}`;
+}
 
+export function isEnter(e: KeyboardEvent): Boolean {
+    return e.key === `Enter`;
 }
