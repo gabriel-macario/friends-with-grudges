16f61c5e1875fe763068323944d69996819797f4
Gabriel Macario <gmacarioweb@gmail.com>
2024-07-24 21:29:46 -0700

    Begin implementation if input flow

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,10 +1,18 @@
-import  React, { CSSProperties, useContext } from "react";
+import  React, { CSSProperties, useContext, useEffect, useState } from "react";
 import GameRow from "./GameRow";
 import { AnswerContext } from "../contexts/GameState"
+import { isLetter } from "../helpers"
 
 interface GameBoardProps {
     gameFinished?: boolean;
 }
+
+interface GameBoardState {
+    currentRow: number,
+    currentString: string,
+    currentWords: Array<string>
+}
+
 const GameBoardStyles: CSSProperties = {
     alignItems: "center",
     display: "flex",
@@ -17,24 +25,60 @@ const GameBoardStyles: CSSProperties = {
 }
 
 const GameBoard: React.FC<GameBoardProps> = ({}: GameBoardProps  ) => {
+    const [boardState, setBoardState] = useState<GameBoardState>({
+        currentRow: 0,
+        currentString: "",
+        currentWords: Array(5).fill("")
+    });
+
+    function handleLetterPress(e: KeyboardEvent) {
+        const {
+            currentRow,
+            currentString,
+            currentWords
+        } = boardState;
+
+        if (currentString.length < 5) {
+            
+            console.log(e.key);
+            
+            let newString: string = currentString + e.key;
+            let newWords: Array<string> = [...currentWords];
+            newWords[currentRow] = newString;
+
+            let newBoardState: GameBoardState = {
+                currentRow,
+                currentString: newString,
+                currentWords: newWords
+            }
+
+            setBoardState(newBoardState)
+        }
+    }
+
+    useEffect(() => {
+        function handleKeyDown(e: KeyboardEvent) {
+            if (isLetter(e)) {
+                handleLetterPress(e)
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyDown)
+
+        return function cleanup() {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+    })
+
     const answer = useContext(AnswerContext);
 
-    const currentWords = [
-        "earth",
-        "story",
-        "adieu",
-        "flops",
-        "quits",
-        "tests"
-    ]
-
-    let gameRows = currentWords.map(word => <GameRow word={word.toUpperCase()} />)
+    let gameRows = boardState.currentWords.map((word, i) => <GameRow key={`${i}`} word={word.toUpperCase()} />)
 
     return <div
         style={GameBoardStyles}
     >
         {gameRows}
-        <div>{`The correct word is "${answer}"`}</div>
+        <div>{`The correct word is ${answer}`}</div>
     </div>
 }
 
