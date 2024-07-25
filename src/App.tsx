dbf3b532ef66055e22e930679b81217917899d57
Gabriel Macario <gmacarioweb@gmail.com>
2024-07-24 21:26:17 -0700

    Utilize use effect in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import { useEffect, useState } from "react";
 import GameBoard from "./components/GameBoard";
 import { AnswerContext, AnswerMapContext } from "./contexts/GameState";
 
 const App: React.FC = () => {
-  const answer = "tests".toUpperCase();
-
-  let answerChars = [...answer];
-
+  // let [answer, setAnswer] = useState("");
+  let answer = "tests".toUpperCase();
   let answerMap = new Map();
 
-  answerChars.forEach(char => answerMap.set(char, true));
+  useEffect(() => {
+    let answerChars = [...answer];
+    answerChars.forEach(char => answerMap.set(char, true));
+  }, [])
 
   return (
-    <AnswerContext.Provider value={answer}>
-      <AnswerMapContext.Provider value = {answerMap}>
+    <AnswerContext.Provider value={answer} >
+      <AnswerMapContext.Provider value={answerMap}>
         <div>
           <h1 style={{
             textAlign: "center"
@@ -20,12 +22,7 @@ const App: React.FC = () => {
           <GameBoard gameFinished={false} />
         </div>
       </AnswerMapContext.Provider>
-    </AnswerContext.Provider>
+    </AnswerContext.Provider >
   )
 }
-// function App() {
-//   return (
-//   )
-// }
-
 export default App
