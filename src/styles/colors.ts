b860e64cec900cd96503b7cc3cbfc8d320fed616
Gabriel Macario <gmacarioweb@gmail.com>
2024-07-25 19:16:07 -0700

    Create colors file

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+interface IColors {
+    lightGrey: string;
+    darkGrey: string;
+    green: string;
+    yellow: string;
+}
+
+export const defaultColors = {
+    lightGrey: "#D3D6DA",
+    darkGrey: "#787C7E",
+    green: "#6AAA64",
+    yellow: "#C9B458"
+}
